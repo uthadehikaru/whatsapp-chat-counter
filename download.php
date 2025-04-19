f1bd4bf8c5d44b1c3fbdc3dae49d801dedd5094c
zuhri utama <zuhriutama@gmail.com>
2025-04-19 11:03:32 +0700

    remove hide text

--- download.php
+++ download.php
@@ -8,27 +8,12 @@ if (!isset($_SESSION['messages']) || empty($_SESSION['messages'])) {
 
 $messages = $_SESSION['messages'];
 
-function hide($text) {
-    if(str_starts_with($text, '+62')) {
-        $parts = explode('-', $text);
-        if (count($parts) > 1) {
-            return trim(end($parts));
-        }
-        return $text;
-    }
-    $parts = explode(' ', $text);
-    if (count($parts) > 1) {
-        return trim(end($parts));
-    }
-    return $text;
-}
-
 // Set headers for file download
 header('Content-Type: text/plain');
 header('Content-Disposition: attachment; filename="chat_export.txt"');
 
 // Output each message
 foreach ($messages as $message) {
-    echo $message['datetime'] . " - " . hide($message['sender']) . ": " . $message['message'] . "\n";
+    echo $message['datetime'] . " - " . $message['sender'] . ": " . $message['message'] . "\n";
 }
 ?> 
